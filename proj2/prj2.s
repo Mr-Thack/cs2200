4b552883d782fc37ba1755c0f1a525fa061353b2
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2026-03-03 17:21:20 -0500

    Finished Project 2B

--- proj2/prj2.s
+++ proj2/prj2.s
@@ -3,124 +3,220 @@
 
 ! vector table
 vector0:
-        .fill 0x00000000                        ! device ID 0
-        .fill 0x00000000                        ! device ID 1
-        .fill 0x00000000                        ! ...
-        .fill 0x00000000
-        .fill 0x00000000
-        .fill 0x00000000
-        .fill 0x00000000
-        .fill 0x00000000                        ! device ID 7
-        ! end vector table
+    .fill 0x00000000                        ! device ID 0
+    .fill 0x00000000                        ! device ID 1
+    .fill 0x00000000                        ! ...
+    .fill 0x00000000
+    .fill 0x00000000
+    .fill 0x00000000
+    .fill 0x00000000
+    .fill 0x00000000                        ! device ID 7
+    ! end vector table
 
-main:	  lea $sp, initsp                         ! initialize the stack pointer
-        lw $sp, 0($sp)                          ! finish initialization
-
-
-        ! TO-DO #1 ========================================================================================================
-        ! Load the address of the timer_handler into the IVT using vector0
-        ! =================================================================================================================
-        add $zero, $zero, $zero
+main:
+    lea $sp, initsp                         ! initialize the stack pointer
+    lw $sp, 0($sp)                          ! finish initialization
 
 
-        ! TO-DO #2 ========================================================================================================
-        ! Load the address of the distance_tracker_handler into the IVT (vector0 was already loaded in to-do #1)
-        ! =================================================================================================================
-        add $zero, $zero, $zero
+    ! TO-DO #1 ========================================================================================================
+    ! Load the address of the timer_handler into the IVT using vector0
+    ! =================================================================================================================
+    lea $t0, timer_handler
+    sw $t0, 0($zero)
 
 
-        lea $t0, minval
-        lw $t0, 0($t0)
-        lea $t1, INT_MAX 			                  ! store 0x7FFFFFFF into minval (to initialize)
-        lw $t1, 0($t1)	                  		
-        sw $t1, 0($t0)
+    ! TO-DO #2 ========================================================================================================
+    ! Load the address of the distance_tracker_handler into the IVT (vector0 was already loaded in to-do #1)
+    ! =================================================================================================================
+    lea $t0, distance_tracker_handler
+    sw $t0, 1($zero)
 
-        ei                                      ! Enable interrupts
 
-        lea $a0, BASE                           ! load base for pow
-        lw $a0, 0($a0)
-        lea $a1, EXP                            ! load power for pow
-        lw $a1, 0($a1)
-        lea $at, POW                            ! load address of pow
-        jalr $at, $ra                           ! run pow
-        lea $a0, ANS                            ! load base for pow
-        sw $v0, 0($a0)
+    lea $t0, minval
+    lw $t0, 0($t0)
+    lea $t1, INT_MAX 			            ! store 0x7FFFFFFF into minval (to initialize)
+    lw $t1, 0($t1)	                  		
+    sw $t1, 0($t0)
 
-        halt                                    ! stop the program here
-        addi $v0, $zero, -1                     ! load a bad value on failure to halt
+    ei                                      ! Enable interrupts
+
+    lea $a0, BASE                           ! load base for pow
+    lw $a0, 0($a0)
+    lea $a1, EXP                            ! load power for pow
+    lw $a1, 0($a1)
+    lea $at, POW                            ! load address of pow
+    jalr $at, $ra                           ! run pow
+    lea $a0, ANS                            ! load base for pow
+    sw $v0, 0($a0)
+
+    halt                                    ! stop the program here
+    addi $v0, $zero, -1                     ! load a bad value on failure to halt
 
 BASE:   .fill 2
 EXP:    .fill 8
-ANS:	  .fill 0                                 ! should come out to 256 (BASE^EXP)
+ANS:	  .fill 0                           ! should come out to 256 (BASE^EXP)
 
 INT_MAX: .fill 0x7FFFFFFF
 
-POW:    addi $sp, $sp, -1                       ! allocate space for old frame pointer
-        sw $fp, 0($sp)
+POW:
+    addi $sp, $sp, -1                       ! allocate space for old frame pointer
+    sw $fp, 0($sp)
 
-        addi $fp, $sp, 0                        ! set new frame pointer
+    addi $fp, $sp, 0                        ! set new frame pointer
 
-        bgt $a1, $zero, BASECHK                 ! check if $a1 is zero
-        beq $zero, $zero, RET1                  ! if the exponent is 0, return 1
+    bgt $a1, $zero, BASECHK                 ! check if $a1 is zero
+    beq $zero, $zero, RET1                  ! if the exponent is 0, return 1
 
-BASECHK:bgt $a0, $zero, WORK                    ! if the base is 0, return 0
-        beq $zero, $zero, RET0
+BASECHK:
+    bgt $a0, $zero, WORK                    ! if the base is 0, return 0
+    beq $zero, $zero, RET0
 
-WORK:   addi $a1, $a1, -1                       ! decrement the power
-        lea $at, POW                            ! load the address of POW
-        addi $sp, $sp, -2                       ! push 2 slots onto the stack
-        sw $ra, -1($fp)                         ! save RA to stack
-        sw $a0, -2($fp)                         ! save arg 0 to stack
-        jalr $at, $ra                           ! recursively call POW
-        add $a1, $v0, $zero                     ! store return value in arg 1
-        lw $a0, -2($fp)                         ! load the base into arg 0
-        lea $at, MULT                           ! load the address of MULT
-        jalr $at, $ra                           ! multiply arg 0 (base) and arg 1 (running product)
-        lw $ra, -1($fp)                         ! load RA from the stack
-        addi $sp, $sp, 2
+WORK:
+    addi $a1, $a1, -1                       ! decrement the power
+    lea $at, POW                            ! load the address of POW
+    addi $sp, $sp, -2                       ! push 2 slots onto the stack
+    sw $ra, -1($fp)                         ! save RA to stack
+    sw $a0, -2($fp)                         ! save arg 0 to stack
+    jalr $at, $ra                           ! recursively call POW
+    add $a1, $v0, $zero                     ! store return value in arg 1
+    lw $a0, -2($fp)                         ! load the base into arg 0
+    lea $at, MULT                           ! load the address of MULT
+    jalr $at, $ra                           ! multiply arg 0 (base) and arg 1 (running product)
+    lw $ra, -1($fp)                         ! load RA from the stack
+    addi $sp, $sp, 2
 
-        beq $zero, $zero, FIN                   ! unconditional branch to FIN
+    beq $zero, $zero, FIN                   ! unconditional branch to FIN
 
-RET1:   add $v0, $zero, $zero                   ! return a value of 0
-	      addi $v0, $v0, 1                        ! increment and return 1
-        beq $zero, $zero, FIN                   ! unconditional branch to FIN
+RET1:
+    add $v0, $zero, $zero                   ! return a value of 0
+	addi $v0, $v0, 1                        ! increment and return 1
+    beq $zero, $zero, FIN                   ! unconditional branch to FIN
 
-RET0:   add $v0, $zero, $zero                   ! return a value of 0
+RET0:
+    add $v0, $zero, $zero                   ! return a value of 0
 
-FIN:	  lw $fp, 0($fp)                          ! restore old frame pointer
-        addi $sp, $sp, 1                        ! pop off the stack
-        jalr $ra, $zero
+FIN:
+    lw $fp, 0($fp)                          ! restore old frame pointer
+    addi $sp, $sp, 1                        ! pop off the stack
+    jalr $ra, $zero
 
-MULT:   add $v0, $zero, $zero                   ! return value = 0
-        addi $t0, $zero, 0                      ! sentinel = 0
-AGAIN:  add $v0, $v0, $a0                       ! return value += argument0
-        addi $t0, $t0, 1                        ! increment sentinel
-        bgt $a1, $t0, AGAIN                     ! while sentinel < argument, loop again
-        jalr $ra, $zero                         ! return from mult
+MULT:
+    add $v0, $zero, $zero                   ! return value = 0
+    addi $t0, $zero, 0                      ! sentinel = 0
+AGAIN:
+    add $v0, $v0, $a0                       ! return value += argument0
+    addi $t0, $t0, 1                        ! increment sentinel
+    bgt $a1, $t0, AGAIN                     ! while sentinel < argument, loop again
+    jalr $ra, $zero                         ! return from mult
 
 
 timer_handler:
+    ! TO-DO #3 ========================================================================================================
+    ! Implement the timer_hander code by first doing handler setup (save $k0, enable interrupts, then save registers).
+    !
+    ! Next, retrieve ticks from memory, increment it by 1, then restore it back into memory
+    !
+    ! Finally, do the handler "teardown" (restore processor state, disable interrupts, then RETI). 
+    ! =================================================================================================================
+    
+    ! Save $k0
+    addi $sp, $sp, -1
+    sw $k0, 0($sp)
+    
+    ! Allow higher priority interrupts
+    ei
 
-        ! TO-DO #3 ========================================================================================================
-        ! Implement the timer_hander code by first doing handler setup (save $k0, enable interrupts, then save registers).
-        !
-        ! Next, retrieve ticks from memory, increment it by 1, then restore it back into memory
-        !
-        ! Finally, do the handler "teardown" (restore processor state, disable interrupts, then RETI). 
-        ! =================================================================================================================
-        add $zero, $zero, $zero
+    ! Save registers
+    addi $sp, $sp, -2
+    sw $t0, 1($sp)
+    sw $t1, 0($sp)
+
+    ! Increment Tick Counter
+    lea $t0, ticks
+    lw $t0, 0($t0)
+    lw $t1, 0($t0)
+    addi $t1, $t1, 1
+    sw $t1, 0($t0)
+
+    ! Stop interrupts
+    di 
+    
+    ! Restore Registers
+    lw $t1, 0($sp)
+    lw $t0, 1($sp)
+    ! Restore $k0
+    lw $k0, 2($sp)
+    addi $sp, $sp, 3
+
+    ! Return to previous instruction
+    reti
+
 
 
 distance_tracker_handler:
+    ! TO-DO #4 ========================================================================================================
+    ! Implement the distance_tracker_handler code by first doing handler setup (save $k0, enable interrupts, then save registers).
+    !
+    ! Then, retrieve the current val from the distance tracker, update maxval and minval accordingly, and then calculate the range
+    !
+    ! Finally, do the handler "teardown" (restore processor state, disable interrupts, then RETI).
+    ! =================================================================================================================
+    ! Save $k0
+    addi $sp, $sp, -1
+    sw $k0, 0($sp)
 
-        ! TO-DO #4 ========================================================================================================
-        ! Implement the distance_tracker_handler code by first doing handler setup (save $k0, enable interrupts, then save registers).
-        !
-        ! Then, retrieve the current val from the distance tracker, update maxval and minval accordingly, and then calculate the range
-        !
-        ! Finally, do the handler "teardown" (restore processor state, disable interrupts, then RETI).
-        ! =================================================================================================================
-        add $zero, $zero, $zero
+    ! Allow higher priority interrupts
+    ei
+
+    ! Save registers
+    addi $sp, $sp, -4
+    sw $s0, 3($sp)
+    sw $t0, 2($sp)
+    sw $t1, 1($sp)
+    sw $t2, 0($sp)
+
+    ! Werk
+    in $s0, 1 ! Put data in $s0
+
+    ! Check Min
+    lea $t0, minval
+    lw $t0, 0($t0)
+    lw $t1, 0($t0)
+    min $t1, $t1, $s0
+    sw $t1, 0($t0)
+
+    ! Check Max
+    lea $t0, maxval
+    lw $t0, 0($t0)
+    lw $t2, 0($t0)
+    max $t2, $t2, $s0
+    sw $t2, 0($t0)
+
+    ! Compute Range
+    nand $t1, $t1, $t1
+    addi $t1, $t1, 1 ! To negate $t1 = min
+    add $s0, $t2, $t1 ! Computes $t2 - $t1 = max - min
+    
+    ! Store Range
+    lea $t0, range
+    lw $t0, 0($t0)
+    sw $s0, 0($t0)
+
+    ! Stop interrupts
+    di 
+
+    ! Restore Registers
+    lw $t2, 0($sp)
+    lw $t1, 1($sp)
+    lw $t0, 2($sp)
+    lw $s0, 3($sp)
+
+    ! Restore $k0
+    lw $k0, 4($sp)
+    addi $sp, $sp, 5
+
+    ! Return to previous instruction
+    reti
 
 
 initsp: .fill 0xA000
